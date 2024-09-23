61a05d1f7787d8430faf3d1e52676fb46b893867
VasukPasuk <vasapaskivskij@gmail.com>
2024-09-23 18:04:14 +0300

    changes: user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,45 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, OnModuleInit} from '@nestjs/common';
 import {PrismaService} from "../utils/prisma.service";
 import {UserPaginationDto} from "./dto/user-pagination.dto";
+import * as bcrypt from "bcrypt";
+import process from "node:process";
 
 @Injectable()
-export class UsersService {
-  constructor(private readonly prisma: PrismaService) {
+export class UsersService implements OnModuleInit {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async onModuleInit() {
+    await this.createFirstAdmin()
   }
+
+  private async createFirstAdmin() {
+    const pass = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10)
+    const admin = await this.prisma.user.upsert({
+      where: {email: process.env.ADMIN_LOGIN},
+      update: {
+        email: process.env.ADMIN_LOGIN,
+        login: process.env.ADMIN_LOGIN,
+        name: process.env.ADMIN_NAME,
+        first_surname: process.env.FIRST_SURNAME,
+        second_surname: process.env.SECOND_SURNAME,
+        password: pass,
+        role: 'ADMIN',
+        activation_link: "*"
+      },
+      create: {
+        email: process.env.ADMIN_LOGIN,
+        login: process.env.ADMIN_LOGIN,
+        name: process.env.ADMIN_NAME,
+        first_surname: process.env.FIRST_SURNAME,
+        second_surname: process.env.SECOND_SURNAME,
+        password: pass,
+        role: 'ADMIN',
+        activation_link: "*"
+      },
+    });
+  }
+
+
 
   async getMany(pagination: UserPaginationDto) {
     const {limit, page, order_rule, field, search} = pagination;
