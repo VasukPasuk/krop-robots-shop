7e487ba3b734ff4880dd1959a76625680c640125
VasukPasuk <vasapaskivskij@gmail.com>
2024-09-24 12:55:12 +0300

    changes: prisma

--- prisma/migrations/20240924095316_init/migration.sql
+++ prisma/migrations/20240924095316_init/migration.sql
@@ -1,5 +1,11 @@
 -- CreateEnum
-CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'CUSTOMER');
+
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PROCESSING', 'FULFILLED');
+
+-- CreateEnum
+CREATE TYPE "Plastic" AS ENUM ('CoPET', 'PLA');
 
 -- CreateTable
 CREATE TABLE "users" (
@@ -11,9 +17,9 @@ CREATE TABLE "users" (
     "login" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "activated" BOOLEAN NOT NULL DEFAULT false,
-    "activation_link" TEXT NOT NULL,
-    "avatar_src" TEXT NOT NULL,
-    "role" "Role" NOT NULL,
+    "activation_link" TEXT,
+    "avatar_src" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'CUSTOMER',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -32,6 +38,32 @@ CREATE TABLE "tags" (
 );
 
 -- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "discount" INTEGER NOT NULL DEFAULT 0,
+    "popular" BOOLEAN NOT NULL DEFAULT false,
+    "description" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL DEFAULT 0,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "category_name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "products_have_Tags" (
+    "product_name" TEXT NOT NULL,
+    "tag_name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_have_Tags_pkey" PRIMARY KEY ("tag_name","product_name")
+);
+
+-- CreateTable
 CREATE TABLE "users_favourite_products" (
     "id" SERIAL NOT NULL,
     "product_name" TEXT NOT NULL,
@@ -43,26 +75,17 @@ CREATE TABLE "users_favourite_products" (
 );
 
 -- CreateTable
-CREATE TABLE "products_have_Tags" (
+CREATE TABLE "reviews" (
     "id" SERIAL NOT NULL,
     "product_name" TEXT NOT NULL,
-    "tag_name" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "products_have_Tags_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "revies" (
-    "id" SERIAL NOT NULL,
-    "product_name" TEXT NOT NULL,
-    "user_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
     "body" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER,
 
-    CONSTRAINT "revies_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -77,28 +100,14 @@ CREATE TABLE "categories" (
 );
 
 -- CreateTable
-CREATE TABLE "products" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "discount" INTEGER NOT NULL DEFAULT 0,
-    "popular" BOOLEAN NOT NULL DEFAULT false,
-    "category_name" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "products_pkey" PRIMARY KEY ("name")
-);
-
--- CreateTable
 CREATE TABLE "variants" (
     "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
     "height" INTEGER NOT NULL,
     "width" INTEGER NOT NULL,
     "length" INTEGER NOT NULL,
     "weight" INTEGER NOT NULL,
     "price" INTEGER NOT NULL,
-    "productName" TEXT,
+    "product_name" TEXT NOT NULL,
     "size_label" TEXT NOT NULL DEFAULT 'Стандартний',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -109,12 +118,17 @@ CREATE TABLE "variants" (
 -- CreateTable
 CREATE TABLE "orders" (
     "id" SERIAL NOT NULL,
-    "customer_id" INTEGER,
-    "customer_contacts" JSONB,
+    "phone_number" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "first_surname" TEXT NOT NULL,
+    "second_surname" TEXT NOT NULL,
+    "comment" TEXT,
+    "payment_type" TEXT NOT NULL,
     "delivery" JSONB NOT NULL,
-    "payment" JSONB NOT NULL,
     "total_price" INTEGER NOT NULL,
     "total_items" INTEGER NOT NULL,
+    "status" "OrderStatus" NOT NULL DEFAULT 'PROCESSING',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -129,6 +143,8 @@ CREATE TABLE "order_items" (
     "color_name" TEXT NOT NULL,
     "variant_id" INTEGER NOT NULL,
     "amount" INTEGER NOT NULL DEFAULT 1,
+    "price" INTEGER NOT NULL,
+    "plastic" "Plastic" NOT NULL DEFAULT 'CoPET',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -138,6 +154,7 @@ CREATE TABLE "order_items" (
 -- CreateTable
 CREATE TABLE "carts" (
     "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
     "total_price" INTEGER NOT NULL,
     "total_items" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -162,10 +179,11 @@ CREATE TABLE "cart_items" (
 -- CreateTable
 CREATE TABLE "photos" (
     "id" SERIAL NOT NULL,
-    "name" TEXT,
+    "name" TEXT NOT NULL,
     "source" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
+    "product_name" TEXT NOT NULL,
 
     CONSTRAINT "photos_pkey" PRIMARY KEY ("id")
 );
@@ -182,6 +200,9 @@ CREATE TABLE "colors" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
@@ -194,25 +215,40 @@ CREATE UNIQUE INDEX "users_activation_link_key" ON "users"("activation_link");
 CREATE UNIQUE INDEX "users_avatar_src_key" ON "users"("avatar_src");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "tags_id_key" ON "tags"("id");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "tags_name_key" ON "tags"("name");
 
 -- CreateIndex
-CREATE INDEX "users_favourite_products_user_id_product_name_idx" ON "users_favourite_products"("user_id", "product_name");
-
--- CreateIndex
-CREATE INDEX "products_have_Tags_tag_name_product_name_idx" ON "products_have_Tags"("tag_name", "product_name");
-
--- CreateIndex
-CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+CREATE UNIQUE INDEX "products_id_key" ON "products"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "products_name_key" ON "products"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "variants_name_key" ON "variants"("name");
+CREATE INDEX "users_favourite_products_user_id_product_name_idx" ON "users_favourite_products"("user_id", "product_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_id_key" ON "categories"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
+-- CreateIndex
+CREATE INDEX "variants_product_name_size_label_idx" ON "variants"("product_name", "size_label");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "carts_user_id_key" ON "carts"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "photos_name_key" ON "photos"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "photos_source_key" ON "photos"("source");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "colors_id_key" ON "colors"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "colors_name_key" ON "colors"("name");
@@ -221,31 +257,28 @@ CREATE UNIQUE INDEX "colors_name_key" ON "colors"("name");
 CREATE UNIQUE INDEX "colors_hex_key" ON "colors"("hex");
 
 -- AddForeignKey
-ALTER TABLE "users_favourite_products" ADD CONSTRAINT "users_favourite_products_product_name_fkey" FOREIGN KEY ("product_name") REFERENCES "products"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "users_favourite_products" ADD CONSTRAINT "users_favourite_products_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "products_have_Tags" ADD CONSTRAINT "products_have_Tags_product_name_fkey" FOREIGN KEY ("product_name") REFERENCES "products"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "products_have_Tags" ADD CONSTRAINT "products_have_Tags_tag_name_fkey" FOREIGN KEY ("tag_name") REFERENCES "tags"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "revies" ADD CONSTRAINT "revies_product_name_fkey" FOREIGN KEY ("product_name") REFERENCES "products"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "revies" ADD CONSTRAINT "revies_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "products" ADD CONSTRAINT "products_category_name_fkey" FOREIGN KEY ("category_name") REFERENCES "categories"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "variants" ADD CONSTRAINT "variants_productName_fkey" FOREIGN KEY ("productName") REFERENCES "products"("name") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "products_have_Tags" ADD CONSTRAINT "products_have_Tags_product_name_fkey" FOREIGN KEY ("product_name") REFERENCES "products"("name") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "orders" ADD CONSTRAINT "orders_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "products_have_Tags" ADD CONSTRAINT "products_have_Tags_tag_name_fkey" FOREIGN KEY ("tag_name") REFERENCES "tags"("name") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users_favourite_products" ADD CONSTRAINT "users_favourite_products_product_name_fkey" FOREIGN KEY ("product_name") REFERENCES "products"("name") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users_favourite_products" ADD CONSTRAINT "users_favourite_products_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_product_name_fkey" FOREIGN KEY ("product_name") REFERENCES "products"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "variants" ADD CONSTRAINT "variants_product_name_fkey" FOREIGN KEY ("product_name") REFERENCES "products"("name") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "order_items" ADD CONSTRAINT "order_items_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -260,6 +293,9 @@ ALTER TABLE "order_items" ADD CONSTRAINT "order_items_color_name_fkey" FOREIGN K
 ALTER TABLE "order_items" ADD CONSTRAINT "order_items_variant_id_fkey" FOREIGN KEY ("variant_id") REFERENCES "variants"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "carts" ADD CONSTRAINT "carts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "cart_items" ADD CONSTRAINT "cart_items_cart_id_fkey" FOREIGN KEY ("cart_id") REFERENCES "carts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -270,3 +306,6 @@ ALTER TABLE "cart_items" ADD CONSTRAINT "cart_items_color_name_fkey" FOREIGN KEY
 
 -- AddForeignKey
 ALTER TABLE "cart_items" ADD CONSTRAINT "cart_items_variant_id_fkey" FOREIGN KEY ("variant_id") REFERENCES "variants"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "photos" ADD CONSTRAINT "photos_product_name_fkey" FOREIGN KEY ("product_name") REFERENCES "products"("name") ON DELETE CASCADE ON UPDATE CASCADE;
